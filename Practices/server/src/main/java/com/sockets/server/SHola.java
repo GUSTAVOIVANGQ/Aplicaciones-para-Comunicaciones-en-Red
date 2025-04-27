44133f668c131aee82dfaeec9ca67184f3b1af49
GUSTAVOIVANGQ <125141006+GUSTAVOIVANGQ@users.noreply.github.com>
2025-04-27 11:36:14 -0600

    test

---
+++ Practices/server/src/main/java/com/sockets/server/SHola.java
@@ -0,0 +1,28 @@
+package com.sockets.server;
+
+import java.net.*;
+import java.io.*;
+
+
+public class SHola {
+    public static void main(String args[]){
+        try{
+            ServerSocket s = new ServerSocket(1234);
+            System.out.println("Esperando cliente...");
+            for(;;){
+                Socket cl = s.accept();
+                System.out.println("Conexion establecida desde " + cl.getInetAddress() + ":" + cl.getPort());
+                String mensaje = "Hola mundo";
+                PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
+                pw.println(mensaje);
+                pw.flush();
+                pw.close();
+                cl.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("");
+        }
+    }
+    
+}
